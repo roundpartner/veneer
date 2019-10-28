550a07fb6d3e40e22187574e61769f233f827433
Tom Lorentsen <toml@giffgaff.co.uk>
2019-10-28 13:59:06 +0000

    List deployments using code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # veneer
 Kubernetes Client
+
+```bash
+export GO111MODULE=on
+go mod init
+```
+
+```bash
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+	var kubeconfig *string
+	var namespace *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	namespace = flag.String("namespace", apiv1.NamespaceDefault, "Specify namespace")
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Kube Config: %s\n", *kubeconfig)
+
+	deploymentsClient := clientset.AppsV1().Deployments(*namespace)
+
+	fmt.Printf("Listing deployments in namespace %q:\n", *namespace)
+	list, err := deploymentsClient.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range list.Items {
+		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+	}
+
+}
