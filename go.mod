5bbd6c31ad20430182caceb4f909e25658bb53bc
Tom Lorentsen <toml@giffgaff.co.uk>
2019-10-29 16:25:49 +0000

    Add memory usage

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/roundpartner/veneer
 go 1.13
 
 require (
+	gopkg.in/inf.v0 v0.9.0
 	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	apiv1 "k8s.io/api/core/v1"
+	"gopkg.in/inf.v0"
+	//apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -13,13 +14,13 @@ import (
 
 func main() {
 	var kubeconfig *string
-	var namespace *string
+	//var namespace *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
-	namespace = flag.String("namespace", apiv1.NamespaceDefault, "Specify namespace")
+	//namespace = flag.String("namespace", apiv1.NamespaceDefault, "Specify namespace")
 	flag.Parse()
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -33,15 +34,89 @@ func main() {
 
 	fmt.Printf("Kube Config: %s\n", *kubeconfig)
 
-	deploymentsClient := clientset.AppsV1().Deployments(*namespace)
-
-	fmt.Printf("Listing deployments in namespace %q:\n", *namespace)
-	list, err := deploymentsClient.List(metav1.ListOptions{})
+	namespaceList, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
-	for _, d := range list.Items {
-		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+
+	top := DataUsage{
+		Name:   ".",
+		Dec:    &inf.Dec{},
+		Memory: &inf.Dec{},
 	}
 
+	for _, ns := range namespaceList.Items {
+
+		namespaceItem := DataUsage{
+			Name:   ns.Name,
+			Dec:    &inf.Dec{},
+			Memory: &inf.Dec{},
+		}
+
+		podClient := clientset.CoreV1().Pods(ns.Name)
+		list, err := podClient.List(metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, d := range list.Items {
+			pod := DataUsage{
+				Name:   d.Name,
+				Dec:    &inf.Dec{},
+				Memory: &inf.Dec{},
+			}
+			for _, c := range d.Spec.Containers {
+				pod.Inner = append(pod.Inner, DataUsage{
+					Name:   c.Name,
+					Dec:    c.Resources.Requests.Cpu().AsDec(),
+					Memory: c.Resources.Requests.Memory().AsDec(),
+				})
+				c.Resources.Requests.Memory()
+				pod.Dec.Add(pod.Dec, c.Resources.Requests.Cpu().AsDec())
+				pod.Memory.Add(pod.Memory, c.Resources.Requests.Memory().AsDec())
+			}
+			namespaceItem.Dec.Add(namespaceItem.Dec, pod.Dec)
+			namespaceItem.Memory.Add(namespaceItem.Memory, pod.Memory)
+			namespaceItem.Inner = append(namespaceItem.Inner, pod)
+		}
+		top.Dec.Add(top.Dec, namespaceItem.Dec)
+		top.Memory.Add(top.Memory, namespaceItem.Memory)
+		top.Inner = append(top.Inner, namespaceItem)
+
+	}
+
+	var dataUsage []DataUsage
+	dataUsage = append(dataUsage, top)
+
+	PrintDu(dataUsage, "")
+
+}
+
+func PrintDu(dataUsage []DataUsage, leftpad string) {
+	if len(dataUsage) == 0 {
+		return
+	}
+	for i, du := range dataUsage {
+		branch := "├── "
+		if i == len(dataUsage)-1 {
+			branch = "└── "
+		} else {
+
+		}
+
+		mem, _ := du.Memory.Unscaled()
+		fmt.Printf("%s%s%s CPU %d MB %s\n", leftpad, branch, du.Dec, mem/1024/1024, du.Name)
+		newleftpad := leftpad + "│    "
+		if i == len(dataUsage)-1 {
+			newleftpad = leftpad + "    "
+		}
+		PrintDu(du.Inner, newleftpad)
+	}
+}
+
+type DataUsage struct {
+	Name   string
+	Dec    *inf.Dec
+	Memory *inf.Dec
+	Inner  []DataUsage
 }
